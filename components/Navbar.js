40a65b31da84105a96c99ea2a29b5a689b0f7ac0
Dodge <34406168+leoddk@users.noreply.github.com>
2025-05-30 12:43:07 +0000

    Stylize Navbar buttons to match Edit and Delete buttons
    - Replace codes from Navbar.js with the new codes
    - Change the CSS styling with this added .nav, .navButton, .navButton:hover

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,12 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/calendar">Calendar</Link>
-      <Link href="/add-eee">Add EEE</Link>
+    <nav className={styles.nav}>
+      <Link href="/" className={`${styles.navButton} ${styles.editButton}`}>Home</Link>
+      <Link href="/calendar" className={`${styles.navButton} ${styles.editButton}`}>Calendar</Link>
+      <Link href="/add-eee" className={`${styles.navButton} ${styles.editButton}`}>Add EEE</Link>
     </nav>
-  )
+  );
 }
